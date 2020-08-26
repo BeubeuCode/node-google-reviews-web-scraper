0806a1cf55cf1d6e7ea9af27fb4acc861c8c5282
Benoît Arn <benoit.arnlt@gmail.com>
2020-08-26 22:31:31 +0200

    removed http but did json thing

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const puppeteer  = require('puppeteer');
-const http = require('http');
 
 const connect = async (url) => {
     const browser = await puppeteer.launch({args: ['--disabled-setuid-sandbox', '--no-sandbox']});
@@ -42,9 +41,16 @@ const connect = async (url) => {
 };
 
 const getReviews = (url) => {
+    let rev;
     connect(url).then((data) => {
-        console.log(JSON.stringify(data));
+       rev = data;
     });
+    return JSON.stringify(rev);
 };
 
-getReviews("https://www.google.fr/maps/place/A+tout+gamer+2.0/@48.9543665,2.8773257,17z/data=!3m1!4b1!4m5!3m4!1s0x47e8a17426f68255:0x6e147b67bf598b81!8m2!3d48.954363!4d2.8795144");
+const server = http.createServer( async (req, res) => {
+    data = await getReviews('https://www.google.com/maps/place/PACA+PROPERTY/@43.3044293,5.3650846,845m/data=!3m2!1e3!4b1!4m5!3m4!1s0x12c9c1e868092f43:0x2ad1fc8819550843!8m2!3d43.3044254!4d5.3672733')
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(data);
+});
