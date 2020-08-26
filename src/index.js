4dad86525b39df5d27101fd4240c08a4840c5b79
Benoît Arn <benoit.arnlt@gmail.com>
2020-08-26 19:31:06 +0200

    hey it's getting the names in an array now

--- src/index.js
+++ src/index.js
@@ -4,16 +4,31 @@ const connect = async (url) => {
     const browser = await puppeteer.launch({args: ['--disabled-setuid-sandbox', '--no-sandbox']});
     const page = await browser.newPage();
     await page.goto(url);
-    await page.waitFor(5000);
+    await page.waitFor(1000);
     const data = await page.evaluate(() => {
-        document.getElementsByClassName('.section-review.content');
+        let reviewAuthorNamesClasses = document.getElementsByClassName('section-review-title');
+        let reviewAuthorNames = [];
+        for (let elements of reviewAuthorNamesClasses) {
+            reviewAuthorNames.push(elements.innerHTML);
+        }
+		let date = document.querySelector('.section-review-publish-date').innerText;
+		let rating = document.querySelector('.section-review-stars').getAttribute("aria-label"); 
+        let reviewContent = document.querySelector('.section-review-text').innerText;
+        return {
+            reviewAuthorNames,
+            date,
+            rating,
+            reviewContent
+        }
     })
+    browser.close();
     return data;
+    
 };
 
 const getReviews = (url) => {
     connect(url).then((data) => {
-        console.log(data);
+        console.table(data);
     });
 };
 
